e33c1623daa33e94206f037702bf7322eeab364d
Aleksey Pravdin <leks16@bk.ru>
2021-09-06 17:48:06 +0300

    2 задание

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/MessageField.jsx';
-
-ReactDom.render(
-    <App />,
-    document.getElementById('app')
-);
